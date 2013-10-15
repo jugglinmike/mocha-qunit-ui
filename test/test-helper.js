680cbdede0c4c86d75b9834734940102d4751af0
Mike Pennisi <mike@mikepennisi.com>
2013-10-14 21:50:18 -0400

    Re-structure project

--- test/test-helper.js
+++
@@ -1,7 +0,0 @@
-var cp = require('child_process');
-
-global.runTestFile = function(testfile, callback) {
-  var n = cp.fork("./testrunner-child.js", {silent:true});
-  n.on('message', callback);
-  n.send({testfile: testfile});
-};
